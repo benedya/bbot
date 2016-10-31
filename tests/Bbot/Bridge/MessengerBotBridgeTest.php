5bf93998e9fc9563c2e33f04c95a8701fa694e36
benedya <benedya@gmail.com>
2016-10-31 13:17:16 +0200

    tests for MessengerBotBridge

--- tests/Bbot/Bridge/MessengerBotBridgeTest.php
+++ tests/Bbot/Bridge/MessengerBotBridgeTest.php
@@ -33,13 +33,87 @@ class MessengerBotBridge extends \PHPUnit_Framework_TestCase
         $this->assertNull(self::$messengerBridge->hideKeyboard('test'));
     }
 
-    public function tstGetUserId()
+    public function testSendImg()
+    {
+        // todo Is there a reason to use url of some img?
+        $this->assertNull(self::$messengerBridge->sendImg(''));
+    }
+
+    public function testGetUserId()
     {
         $this->assertEquals(1, self::$messengerBridge->getUserId());
+    }
+
+    public function testGetUserProfile()
+    {
+        $this->assertArrayHasKey('id', self::$messengerBridge->getUserProfile());
     }
 
     public function testSendText()
     {
         $this->assertNull(self::$messengerBridge->sendText('test'));
+    }
+
+    /**
+     * @dataProvider buttonsProvider
+     */
+    public function testSendButtons($buttons)
+    {
+        $this->assertNull(self::$messengerBridge->sendButtons([
+            'caption' => 'Hey there!',
+            'buttons' => $buttons,
+        ]));
+    }
+
+    /**
+     * @dataProvider buttonsProvider
+     */
+    public function testBuildButtons($buttons)
+    {
+        $this->assertInternalType('array', self::$messengerBridge->buildButtons($buttons));
+    }
+
+    /**
+     * @dataProvider buttonsProvider
+     */
+    public function testBuildItemWithButtons($buttons)
+    {
+        $this->assertInstanceOf(
+            \pimax\Messages\MessageElement::class,
+            self::$messengerBridge->buildItemWithButtons(
+                [
+                    'title' => 'Build something for me',
+                    'subtitle' => 'See up',
+                ],
+                $buttons
+            )
+        );
+    }
+
+    /**
+     * @dataProvider buttonsProvider
+     */
+    public function testSendListItems($buttons)
+    {
+        $listItems = [];
+        $listItems[] = self::$messengerBridge->buildItemWithButtons(
+            [
+                'title' => 'Build something for me',
+                'subtitle' => 'See up',
+            ],
+            $buttons
+        );
+        $this->assertNull(self::$messengerBridge->sendListItems($listItems));
+    }
+
+    public function buttonsProvider()
+    {
+        $buttons = [];
+        $buttons[] = [
+            'type' => 'postback',
+            'title' => 'Show me the google',
+            'url' => 'http://google.com'
+        ];
+        return [[$buttons]];
     }
 }
