f04d51bf163a27ecb736bba99bb6f2a711d5b82c
TurBo <benedya@gmail.com>
2018-05-07 22:50:21 +0300

    Removed unused function from logger

--- src/Bbot/CliLogger.php
+++ src/Bbot/CliLogger.php
@@ -27,20 +27,4 @@ class CliLogger extends AbstractLogger
     {
         return 'cli' === php_sapi_name();
     }
-
-    protected function printArray(array $data)
-    {
-        echo "\n---- start print array ----\n";
-        foreach ($data as $k => $v) {
-            echo "\n -> ".$k;
-            if (is_array($v)) {
-                foreach ($v as $v2) {
-                    echo ' : '.$v2.'; ';
-                }
-            } else {
-                echo ': '.$v." \n";
-            }
-        }
-        echo "\n---- end print array ----\n";
-    }
 }
