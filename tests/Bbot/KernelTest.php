449573795dd5befa22584fba9fd9227bdefaab6c
TurBo <benedya>
2018-06-05 22:27:30 +0200

    Added KernelTest

--- tests/Bbot/KernelTest.php
+++ tests/Bbot/KernelTest.php
@@ -3,12 +3,34 @@
 namespace Bbot\Tests;
 
 use Bbot\Kernel;
+use Bbot\Provider\AppProvider;
 use Bbot\Request\TelegramRequest;
+use Psr\Log\NullLogger;
 
 class KernelTest extends \PHPUnit_Framework_TestCase
 {
-    public function testHandleTextRequest()
+    protected $exampleTelegramRequestMsg = '{"update_id":1,"channel_post":{"message_id":1,"chat":{"id":-1,"title":"Title","username":"username","type":"channel"},"date":1527529238,"text":"Since yesterday"}}';
+
+    /**
+     * @dataProvider kernelProvider
+     */
+    public function testHandleTelegramRequest(Kernel $kernel, TelegramRequest $request)
     {
-        // todo implement depends on bot
+        $this->assertNull($kernel->handle($request));
+    }
+
+    public function kernelProvider()
+    {
+        return [
+            [(new Kernel(
+                [new AppProvider()],
+                new \Bbot\Bridge\TelegramBot(
+                    'key',
+                    -1,
+                    new NullLogger()
+                )
+            )), new \Bbot\Request\TelegramRequest(json_decode($this->exampleTelegramRequestMsg, true))]
+            // todo implement kernels for others bot-platforms
+        ];
     }
 }
